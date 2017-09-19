d5268628090fbd71cd5ed459357dc7d4f08161ca
Catherine Liu <catherineqliu@outlook.com>
2017-09-19 01:00:09 -0700

    Embedded google maps

--- src/components/Children/Contact/Map.jsx
+++ src/components/Children/Contact/Map.jsx
@@ -11,7 +11,7 @@ const Map = () => {
   return (
   	<div className="contact-map pull-right col-md-4 nopadding" id="contact-map">
     {/*Write code here */}
-    	<img className="pull-right" src="http://lorempixel.com/g/550/550/" />
+    	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13333.5724243279!2d-111.8427326!3d33.3346784!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc0a706c582eae531!2sMambo&#39;s+Dominican+Kitchen!5e0!3m2!1sen!2sus!4v1505807815771" width="600" height="450" frameBorder="0" style={{border:0}} allowFullScreen></iframe>
     </div>
   );
 }
