dfa6946a4bd6c266088682b4be7dc1635c622d8f
nverges <nverges@email.arizona.edu>
2017-09-26 09:56:52 -0700

    fixed links, footer r display, color

--- src/components/Children/Footer.jsx
+++ src/components/Children/Footer.jsx
@@ -15,12 +15,12 @@ const Footer = (props) => {
           <footer>
 
               <ul className="list-inline">
-                  <li><a href="/">Home</a></li>
-                  <li><a href='#'>About </a></li>
-                  <li><a href='#'>Menu</a></li>
-                  <li><a href="#">Social </a></li>
-                  <li><a href="#">Contact </a></li>
-                  <li><a href='#'>Manager Portal </a></li>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#section-about">About </a></li>
+                  <li><a href="#section-menu">Menu</a></li>
+                  <li><a href="#section-social">Social </a></li>
+                  <li><a href="#section-contact">Contact </a></li>
+                  <li><a href="#">Manager Portal </a></li>
               </ul>
               <p className="copyright">Catherine Liu, Cesar Perez, Nick Verges © 2017</p>
 
