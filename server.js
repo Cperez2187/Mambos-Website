bf1b8e2bf3f7b4220be153cab358056056f075be
nverges <nverges@email.arizona.edu>
2017-09-29 16:46:27 -0700

    index static

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = require('./models');
 // const Sequelize = require('sequelize');
 
 // Static directory
-app.use('/', express.static("public"));
+app.use('/index', express.static("public"));
 
 // Run Morgan for Logging
 app.use(logger("dev"));
